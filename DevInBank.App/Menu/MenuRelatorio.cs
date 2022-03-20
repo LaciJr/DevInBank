6e7095f2256dc96f8951d333767ab84345be5730
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-03-20 16:20:53 -0300

    Finaliza menu relatorio

--- DevInBank.App/Menu/MenuRelatorio.cs
+++ DevInBank.App/Menu/MenuRelatorio.cs
@@ -16,6 +16,7 @@ namespace DevInBank.App.Menu
             {
                 int i = 0;
                 Console.WriteLine("Relatórios:");
+                Console.WriteLine($"{++i}. Listar todas as contas");
                 Console.WriteLine($"{++i}. Histórico de transferências");
                 Console.WriteLine($"{++i}. Contas com saldo negativo");
                 Console.WriteLine($"{++i}. Total do valor investido");
@@ -28,20 +29,30 @@ namespace DevInBank.App.Menu
                 {
                     case "1":
                         Console.Clear();
-                        HistoricoTransferencia.GetHistoricoTransferencia();
+                        ListarContas();
                         Utilitario.OperacaoRealizada();
                         break;
                     case "2":
                         Console.Clear();
+                        Console.WriteLine("HISTÓRICO DE TRANSFERÊNCIAS");
+                        HistoricoTransferencia.GetHistoricoTransferencia();
+                        Utilitario.OperacaoRealizada();
                         break;
                     case "3":
                         Console.Clear();
+                        ListarContasNegativas();
+                        Utilitario.OperacaoRealizada();
                         break;
                     case "4":
                         Console.Clear();
-                        MenuPrincipal.MenuInicial();
+                        TotalInvestido();
+                        Utilitario.OperacaoRealizada();
                         break;
                     case "5":
+                        Console.Clear();
+                        MenuPrincipal.MenuInicial();
+                        break;
+                    case "6":
                         Console.WriteLine("Encerrando aplicação...");
                         Environment.Exit(0);
                         break;
@@ -50,6 +61,53 @@ namespace DevInBank.App.Menu
                         Console.WriteLine($"{seletor} não é uma opção válida");
                         break;
                 }
+            }
+        }
+
+        static void TotalInvestido()
+        {
+            decimal totalInvestido = 0;
+
+            foreach (var item in ContaPoupanca.ListaContasPoupanca)
+            {
+                totalInvestido += item.GetSaldo();
+            }
+
+            foreach (var item in ContaInvestimento.ListaContasInvestimento)
+            {
+                totalInvestido += item.ValorAplicado;
+            }
+
+            Console.WriteLine($"O valor total Investido no momento é de R${totalInvestido}");
+        }
+
+        static void ListarContas()
+        {
+            Console.WriteLine("CONTAS CADASTRADAS");
+
+            foreach (var item in Conta.ListaContas)
+            {
+                Console.WriteLine($"Tipo: {item.TipoConta}");
+                Console.WriteLine($"Número: {item.NumConta}");
+                Console.WriteLine($"Titular: {item.Nome}");
+                Console.WriteLine($"Agencia: {item.Agencia}");
+                Console.WriteLine("-----------------------------");
+            }
+        }
+
+        static void ListarContasNegativas()
+        {
+            Console.WriteLine("CONTAS COM SALDO NEGATIVO");
+
+            var contasNegativas = Conta.ListaContas.FindAll(e => e.GetSaldo() < 0);
+
+            foreach (var item in contasNegativas)
+            {
+                Console.WriteLine($"Tipo: {item.TipoConta}");
+                Console.WriteLine($"Número: {item.NumConta}");
+                Console.WriteLine($"Titular: {item.Nome}");
+                Console.WriteLine($"Agencia: {item.Agencia}");
+                Console.WriteLine("-----------------------------");
             }
         }
     }
